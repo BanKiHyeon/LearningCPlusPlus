8188f3f866a0c88a4f3fbcd73b402662572ba9bb
BanKiHyeon <BanKiHyeon@gmail.com>
2024-03-10 00:21:12 +0900

    Update namespace.cpp

--- cpp20/namespace/namespace.cpp
+++ cpp20/namespace/namespace.cpp
@@ -1,6 +1,36 @@
 ﻿import <iostream>;
 
+namespace mycode {
+	void foo()
+	{
+		std::cout << "foo() called in the mycode namespace" << std::endl;
+	}
+}
+
+namespace a::b::c {
+	void foo()
+	{
+		std::cout << "foo() called in the a::b::c namespace" << std::endl;
+	}
+}
+
+namespace a {
+	namespace b {
+		void foo()
+		{
+			std::cout << "foo() called in the a::b namespace" << std::endl;
+		}
+	}
+}
+
+using namespace mycode;
+namespace Sum = a::b::c;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	mycode::foo();
+	foo();			
+	a::b::c::foo();
+	Sum::foo();
+	a::b::foo();
 }
