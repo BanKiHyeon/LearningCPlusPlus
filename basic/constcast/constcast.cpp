652c1f50a9f27a642c0740974f547212ef717009
BanKiHyeon <BanKiHyeon@gmail.com>
2024-04-06 23:19:07 +0900

    add const cast

---
+++ basic/constcast/constcast.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>;
+
+void ThirdPartyLibraryMethod(char* str) {
+	std::cout << str << std::endl;
+}
+
+void f(const char* str)
+{
+	ThirdPartyLibraryMethod(const_cast<char*>(str));
+	
+	// error
+	//ThirdPartyLibraryMethod(str);
+	//ThirdPartyLibraryMethod("zxc");
+}
+
+int main()
+{
+	f("Hello World");
+}
