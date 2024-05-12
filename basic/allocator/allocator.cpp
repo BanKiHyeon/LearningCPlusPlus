5303a822a7b02013aeb262209fd9d4e72c52e442
BanKiHyeon <BanKiHyeon@gmail.com>
2024-05-12 22:52:19 +0900

    add allocator

---
+++ basic/allocator/allocator.cpp
@@ -0,0 +1,19 @@
+﻿#include <memory>
+
+using namespace std;
+
+class MyClass {};
+
+int main()
+{
+	allocator<MyClass> alloc;
+
+	auto* memory{ alloc.allocate(1) };
+
+	new(memory) MyClass{};
+
+	destroy_at(memory);
+
+	alloc.deallocate(memory, 1);
+	memory = nullptr;
+}
